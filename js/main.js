a07988eda3128f0fd0bce986385cff1892c44c30
Winnie Lau <winlaud@gmail.com>
2014-01-30 07:47:12 -0500

    added slideshow

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var $items = $('.carousel-items img');
+var $dialog = $('#video')
+var $placeholder = $('#video-placeholder')
 
 var switchItems = function(outgoing, incoming) {
 	$items.eq(incoming).attr('data-state', 'incoming').fadeIn(250, function(){
@@ -32,4 +34,16 @@ $('.prev').on('click', function () {
 	
 	switchItems(current, prev);
 
+});
+
+dialogPolyfill.registerDialog($dialog.get(0));
+
+$('#btn-open').on('click', function () {
+	$placeholder.html('<iframe class="vid" src="http://player.vimeo.com/video/81360763?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+	$dialog.get(0).showModal();
+});
+
+$('#btn-close').on('click', function () {
+	$placeholder.html('');
+	$dialog.get(0).close();
 });
