24b1d0ba5fbb14c7e0c63e5e4a2b97d71a271f5c
Winnie Lau <winlaud@gmail.com>
2014-02-10 09:26:02 -0500

    Revert "Added testimonials.js and .json"
    This reverts commit 1536330c72159ef3f4d7221d558ecda69d111546.

--- js/testimonials.js
+++
@@ -1,8 +0,0 @@
-
-var userData = $.ajax('js/testimonials.json', {
-	dataType: 'json'
-});
-userData.done(function (data){
-	var say = Math.round( Math.random() * data.length - 1);
-	$('.text-here').html(data[say].quote);
-});
