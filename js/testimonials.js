ce30d7b26ffa903ae008c79b06a83a7d27bb2983
Winnie Lau <winlaud@gmail.com>
2014-02-13 08:04:53 -0500

    separated quote with who said it

--- js/testimonials.js
+++ js/testimonials.js
@@ -5,4 +5,5 @@ var userData = $.ajax('js/testimonials.json', {
 userData.done(function (data){
 	var say = Math.round( Math.random() * data.length - 1);
 	$('.text-here').html(data[say].quote);
+	$('.who').html(data[say].by);
 });
